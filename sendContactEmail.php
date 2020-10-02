720be821dd3bb60c11feb70a6dab100d3e03b60a
Faskomilo <oscarve.do@outlook.com>
2020-10-02 02:06:17 -0500

    php email working just freaking fine

--- sendContactEmail.php
+++ sendContactEmail.php
@@ -1,27 +1,30 @@
 <?php
-$request = json_decode(file_get_contents("php://input"), true);
-$success = "ko";
-$sanitized = filter_var($request);
-if($request != null && filter_var($request, $sanitized)){
+$request = json_decode(file_get_contents('php://input'), true);
+$success = 'ko';
+$sanitized = filter_var_array($request, FILTER_SANITIZE_STRING);
+echo json_encode($sanitized);
+if($request != null && filter_var_array($request, $sanitized)){
     $to = null;
     $subject = null;
     $body = null;
     try{
-        $from = $request["email"];
-        $subject = $request["subject"];
-        $body = $request["body"];
+        $from = $request['email'];
+        $subject = $request['subject'];
+        $body = $request['body'];
+        echo 'pene';
         if($from != null && $subject != null && $body != null){
-            $toEmail = "customerservice@commercial-cjs.com";
-            $htmlContent  = "<h2>Contact Request Submited</h2>
-            <h4>Email<h4><p>".$from."</p>
-            <h4>Subject<h4><p>".$subject."</p>
-            <h4>Message<h4><p>".$body."</p>";
-            $headers = "MIME-Version: 1.0\r\n";
-            $headers .= "Content-type:text/html;charset=UTF-8\r\n";
-            $headers .= "From: Name<".$from.">\r\n";
-
+            $toEmail = 'oscarve.do@outlook.com';
+            $htmlContent  = '<h2>Contact Request Submited</h2>
+            <h4>Email<h4><p>'.$from.'</p>
+            <h4>Subject<h4><p>'.$subject.'</p>
+            <h4>Message<h4><p>'.$body.'</p>';
+            $headers = 'From: '.$from;
             if(mail($toEmail, $subject, $htmlContent, $headers)){
-                $success = "ok";
+                
+                $success = 'ok';
+            }
+            else{
+                echo 'es un pene';
             }
         }
     }
@@ -30,8 +33,7 @@ if($request != null && filter_var($request, $sanitized)){
     }
 }
 
-$response[] = array(
-    "success" => $success
-);
-echo json_encode($response)
+$response = [
+    'success' => $success];
+//echo json_encode($response)
 ?>
